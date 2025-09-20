6669bf62123c87f3aedb3490eb65962f59a24a4c
Acidburn07 <sandesh.bishokarma@student.hamk.fi>
2025-09-21 01:03:00 +0545

    Added Main.java program for sum of numbers

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Ask for the first number
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Ask for the first number
+            System.out.println("First number?");
+            int num1 = scanner.nextInt();
+            
+            // Ask for the second number
+            System.out.println("Second number?");
+            int num2 = scanner.nextInt();
+            
+            // Print the formula
+            System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        }
+    }
+}
